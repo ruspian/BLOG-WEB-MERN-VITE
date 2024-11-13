11fcd3b0be6b20cb26d307d525af0f6e3c411f6a
ruspian <ruspianntb@gmail.com>
2024-11-13 10:17:52 +0800

    mengonekkan project dengan database

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+// RPdBb3VB0K3FW9nh
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database berhasil terhubung");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
